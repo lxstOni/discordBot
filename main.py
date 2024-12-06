bef9e51fcfe3b54d5da97bc202e7955a494ba37c
lxstOni <174956003+lxstOni@users.noreply.github.com>
2024-12-06 11:49:14 +0000

    VotingSystem is broken :c

--- main.py
+++ main.py
@@ -18,7 +18,17 @@ bot = ezcord.Bot(
 )
 bot.add_help_command()
 
+
+def log_folder_exist():
+    folder_path = '../log/'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Nested folders '{folder_path}' created.")
+    else:
+        print(f"Nested folders '{folder_path}' already exist.")
+
 if __name__ == "__main__":
     bot.load_cogs("cogs")
     settings.setup_logger()
     bot.run(os.getenv("TOKEN"))
+    log_folder_exist()
